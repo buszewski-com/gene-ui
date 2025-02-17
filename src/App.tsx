dee892de5000859de22972bb2bad166f0fab3abf
Tomasz Buszewski <tomek.buszewski@gmail.com>
2025-02-17 19:40:09 +0100

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <main>
+      <h1 className="text-4xl text-center mb-4">Hello</h1>
+      <p className="text-center">
+        Run <code>storybook</code> to start working
+      </p>
+    </main>
+  );
+}
