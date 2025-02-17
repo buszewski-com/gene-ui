2085d470771ee58f93072be2ea5df61dbe63f36f
Tomasz Buszewski <tomek.buszewski@gmail.com>
2025-02-17 19:43:39 +0100

    feat: tailwind plugin for prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 80,
+  bracketSpacing: true,
+  allowParent: "avoid",
+  proseWrap: "always",
+};
