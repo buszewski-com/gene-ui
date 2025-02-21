03d8c60e85d52e4a498628b2867f7f658c79ac36
Tomasz Buszewski <tomek.buszewski@gmail.com>
2025-02-21 17:58:25 +0100

    feat: npm package

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 import type { UserConfigExport } from "vitest/config";
@@ -6,14 +7,26 @@ import type { UserConfigExport } from "vitest/config";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "Gene",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./config/test.setup.ts"],
-    coverage: {
-      enabled: true,
-      exclude: ["src/**/*.stories.tsx", "src/**/index.ts"],
-      include: ["src/{ui,hooks}/**/*.{tsx,ts}"],
-    },
   },
 }) satisfies UserConfigExport;
